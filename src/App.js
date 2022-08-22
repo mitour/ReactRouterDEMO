b76b36b3fe94bc5422284dbe80f6f57c60dfa471
Mitour <love2082020@gmail.com>
2022-08-22 10:57:08 +0800

    install sass package

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import { Routes, Route } from "react-router-dom";
-import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import "./css/App.scss";
 import Home from "./components/Home";
 import About from "./components/About";
 import FAQ from "./components/FAQ";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <nav className="navbar">
+        <ul>
+          <li>
+            <Link to="/">首頁</Link>
+          </li>
+          <li>
+            <Link to="about">關於我</Link>
+          </li>
+          <li>
+            <Link to="faq">常見問答</Link>
+          </li>
+        </ul>
+      </nav>
       <h1>Welcome to 高雄旅遊網</h1>
       <Routes>
         <Route path="/" element={<Home />} />
