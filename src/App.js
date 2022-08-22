be54ae2e3e1bee194d6e9bef0bd703384c0acf76
Mitour <love2082020@gmail.com>
2022-08-22 14:15:10 +0800

    navbar

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faOtter } from "@fortawesome/free-solid-svg-icons";
 import { Routes, Route, Link } from "react-router-dom";
 import "./css/App.scss";
 import Home from "./components/Home";
@@ -6,27 +8,34 @@ import FAQ from "./components/FAQ";
 
 function App() {
   return (
-    <div>
-      <nav className="navbar">
-        <ul>
-          <li>
-            <Link to="/">首頁</Link>
-          </li>
-          <li>
-            <Link to="about">關於我</Link>
-          </li>
-          <li>
-            <Link to="faq">常見問答</Link>
-          </li>
-        </ul>
-      </nav>
-      <h1>Welcome to 高雄旅遊網</h1>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="faq" element={<FAQ />} />
-      </Routes>
-    </div>
+    <>
+      <header className="navbar">
+        <nav className="wrap d-flex align-center">
+          <Link className="d-flex align-center" to="/">
+            <FontAwesomeIcon className="logo" icon={faOtter} />
+            <h1>高雄旅遊網</h1>
+          </Link>
+          <ul className="d-flex flex-end">
+            <li>
+              <Link to="/">首頁</Link>
+            </li>
+            <li>
+              <Link to="about">關於我</Link>
+            </li>
+            <li>
+              <Link to="faq">常見問答</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main className="wrap">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="faq" element={<FAQ />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
